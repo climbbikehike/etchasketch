aab4278803f71125fae87331c95bab610be8c1da
lorin <lorinelizebethxo@gmail.com>
2016-12-04 11:06:04 -0500

    Beginnings of the div grid.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+function drawGrid(height,width) {
+	//var singleDiv = document.createElement('div');
+
+	for (i=0; i < height; i++) {
+		$('<div>').appendTo('.container');
+	};
+}
+
+
 $(document).ready(function(){
-    $("div").click(function(){
-        $("div").hide();
-    });
+	var height = 16;
+	var width = 16;
+
+	drawGrid(height,width);
+
+
+   /*$(".divTable").click(function(){
+        $(".divTable").hide();
+   });*/
 });
